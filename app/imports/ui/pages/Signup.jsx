eec3476c106162228b829e785d0779e44ce426e2
Mujtaba Abdullah Quadri <mujtabaq@hawaii.edu>
2021-10-16 13:35:59 -1000

    making the username field an entered username seperate from the email

--- app/imports/ui/pages/Signup.jsx
+++ app/imports/ui/pages/Signup.jsx
@@ -11,7 +11,7 @@ class Signup extends React.Component {
   /* Initialize state fields. */
   constructor(props) {
     super(props);
-    this.state = { email: '', password: '', error: '', redirectToReferer: false };
+    this.state = { email: '', username: '', password: '', error: '', redirectToReferer: false };
   }
 
   /* Update the form controls each time the user interacts with them. */
@@ -21,8 +21,8 @@ class Signup extends React.Component {
 
   /* Handle Signup submission. Create user account and a profile entry, then redirect to the home page. */
   submit = () => {
-    const { email, password } = this.state;
-    Accounts.createUser({ email, username: email, password }, (err) => {
+    const { email, username, password } = this.state;
+    Accounts.createUser({ email, username, password }, (err) => {
       if (err) {
         this.setState({ error: err.reason });
       } else {
@@ -55,6 +55,16 @@ class Signup extends React.Component {
                   name="email"
                   type="email"
                   placeholder="E-mail address"
+                  onChange={this.handleChange}
+                />
+                <Form.Input
+                  label="Username"
+                  id="signup-form-username"
+                  icon="pen square"
+                  iconPosition="left"
+                  name="username"
+                  type="username"
+                  placeholder="The name of your account"
                   onChange={this.handleChange}
                 />
                 <Form.Input
