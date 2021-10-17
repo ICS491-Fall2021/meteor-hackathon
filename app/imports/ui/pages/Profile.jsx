bf0ba0923424aa7d62d60c9016204d2057d8136d
Jolie <joliec@hawaii.edu>
2021-10-16 17:44:00 -1000

    persist schedule state

--- app/imports/ui/pages/Profile.jsx
+++ app/imports/ui/pages/Profile.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Grid, Segment, Button, Header } from 'semantic-ui-react';
+import { Grid, Segment, Button, Form, Header } from 'semantic-ui-react';
 import ScheduleSelector from 'react-schedule-selector'
+
 
 class Profile extends React.Component {
   constructor(props) {
       super(props);
-      this.state = {
+      this.state =  {
         user: this.props.currentUser,
         schedule: [new Date('October 17, 2021 12:30:00'), new Date('October 18, 2021 12:30:00')],
       };
@@ -16,11 +17,8 @@ class Profile extends React.Component {
         console.log(this.state.schedule);
     });
   }
-
-  objectReformatDate
-  
-
   render() {
+
     return (
       <Grid columns={2} relaxed padded className="wrapping">
           <Grid.Row stretched>
@@ -39,14 +37,28 @@ class Profile extends React.Component {
               </Grid.Column>
               <Grid.Column className="box-color" width={3}>
                   <Header as='h2'>Groups</Header>
+                  <Header as='h2'>Contacted</Header>
               </Grid.Column>
           </Grid.Row>
           <Grid.Row stretched>
               <Grid.Column className="box-color" width={12}>
                  <Header as='h2'>Interests</Header>
+                  <Form>
+                    <Form.Group grouped>
+                      <label>Add your Interests</label>
+                      <Form.Field label='This one' control='input' type='checkbox' />
+                      <Form.Field label='That one' control='input' type='checkbox' />
+                    </Form.Group>
+                    <Form.Group widths='equal'>
+                      <Form.Field label='Input your interest' control='input' />
+                    </Form.Group>
+                    <Form.Field control='button'>
+                      Submit
+                    </Form.Field>
+                </Form>
+
               </Grid.Column>
-              <Grid.Column className="box-color" width={3}>
-                <Header as='h2'>Contacted</Header>
+              <Grid.Column className="box-color" width={12}>
               </Grid.Column>
           </Grid.Row>
       </Grid>
