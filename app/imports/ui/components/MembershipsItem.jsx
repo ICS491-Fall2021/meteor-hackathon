622ec9235961bfafb14cbeef532be75821d4b3b7
Jolie <joliec@hawaii.edu>
2021-10-18 01:19:48 -1000

    Added membership client

--- app/imports/ui/components/MembershipsItem.jsx
+++ app/imports/ui/components/MembershipsItem.jsx
@@ -4,24 +4,24 @@ import PropTypes from 'prop-types';
 import { withRouter, Link } from 'react-router-dom';
 
 /** Renders a single row in the List Status table. See pages/ListStatus.jsx. */
-class InterestItem extends React.Component {
+class MembershipsItem extends React.Component {
   render() {
     return (
       <Table.Row>
-        <Table.Cell>{this.props.interest.interests}</Table.Cell>
+        <Table.Cell>{this.props.membership.groupID}</Table.Cell>
       </Table.Row>
     );
   }
 }
 
 // Require a document to be passed to this component.
-InterestItem.propTypes = {
-  interest: PropTypes.shape({
-    owner: PropTypes.string,
-    interest: PropTypes.string,
+MembershipsItem.propTypes = {
+  membership: PropTypes.shape({
     _id: PropTypes.string,
+    userID: PropTypes.string,
+    groupID: PropTypes.string,
   }).isRequired,
 };
 
 // Wrap this component in withRouter since we use the <Link> React Router element.
-export default withRouter(InterestItem);
+export default withRouter(MembershipsItem);
