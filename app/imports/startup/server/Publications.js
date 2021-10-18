151de620869dc37f0070b13fac4f8328a6b774b9
Dylan Decker <dylaned@hawaii.edu>
2021-10-17 17:56:04 -1000

    Create group publication

--- app/imports/startup/server/Publications.js
+++ app/imports/startup/server/Publications.js
@@ -1,6 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import { Roles } from 'meteor/alanning:roles';
 import { Stuffs } from '../../api/stuff/Stuff';
+import { Groups } from '../../api/group/Group';
 
 // User-level publication.
 // If logged in, then publish documents owned by this user. Otherwise publish nothing.
@@ -14,9 +15,9 @@ Meteor.publish(Stuffs.userPublicationName, function () {
 
 // Admin-level publication.
 // If logged in and with admin role, then publish all documents from all users. Otherwise publish nothing.
-Meteor.publish(Stuffs.adminPublicationName, function () {
+Meteor.publish(Groups.adminPublicationName, function () {
   if (this.userId && Roles.userIsInRole(this.userId, 'admin')) {
-    return Stuffs.collection.find();
+    return Groups.collection.find();
   }
   return this.ready();
 });
