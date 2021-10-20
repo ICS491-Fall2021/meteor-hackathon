dbe28f1dcfbd73178484f2fb18f89c27522696cd
Mujtaba Abdullah Quadri <mujtabaq@hawaii.edu>
2021-10-20 02:24:45 -1000

    fixing bug not allowing multiple users into group

--- app/imports/ui/pages/Group.jsx
+++ app/imports/ui/pages/Group.jsx
@@ -104,21 +104,23 @@ getDates() {
 }
 
 findallMembers(theGroupID) {
- const result = [];
- const username = [];
+ let result = [];
+ let username = [];
   const members = Memberships.collection.find({ groupID: this.props.theGroupPageID }).fetch();
   const names = members.map(a => a.userID);
   // console.log(`WAWWW${JSON.stringify(names)}`);
-
-  for (let i = 0; i < names.length; i++) {
-     result.push(Meteor.users.find({ _id: names[i] }).fetch());
+  result = Availabilities.collection.find({'owner':{'$in':names}}).fetch();
+  for (let i = 0; i < result.length; i++) {
+     username.push(result[i].ownername);
   }
 
+  /*
   for (let i = 0; i < result.length; i++) {
     const groupObject = result[0][i];
     const groupArray = Object.values(groupObject)[2];
      username.push(groupArray);
    }
+   */
   return username;
 
   // Can't access other people's availabilities... maybe add another field into memberships collection to have a name
